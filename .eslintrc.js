c96c901bd44fb7bfb33b213e9836b4266ad0ef49
Mariia Ushakova <m.ushakova22@gmail.com>
2023-10-04 14:46:53 +0200

    init: add wdio and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'mocha': true
+  },
+  'extends': 'google',
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  "ignorePatterns": ["wdio.conf.js"],
+  'rules': {
+  },
+};
